01ed513e4311157c9e222283a18533d2e2508cdb
Khushi-868 <ka587183@gmail.com>
2024-07-16 07:20:51 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Footer from './components/Footer';
+import AddItem from './components/AddItem';
+function App() {
+  const products=[
+    {
+      price:99999,
+      name:"I Phone 10s Max",
+      quantity:0,
+    },
+    {
+      price:99999,
+      name:"Redmi Note 10S Max",
+      quantity:0,
+    },
+    {
+      price:99999,
+      name:"POCO",
+      quantity:0,
+    }
+  ]
+  let [productList,setProductList]=useState(products);
+  let [totalAmount,setTotalAmount]=useState(0);
+  const incrementQuantity=(index)=>{
+    let newProductList=[...productList];
+    let newTotalAmount=totalAmount;
+    newProductList[index].quantity++;
+    newTotalAmount+=newProductList[index].price;
+    setTotalAmount(newTotalAmount);
+    setProductList(newProductList);
+  };
+  const decrementQuantity=(index)=>{
+    let newProductList=[...productList];
+    let newTotalAmount = totalAmount;
+    if(newProductList[index].quantity>0)
+    {
+    newProductList[index].quantity--;
+    newTotalAmount-=newProductList[index].price;
+    }
+    setTotalAmount(newTotalAmount);
+    setProductList(newProductList);
+  };
+  const resetQuantity = () => {
+    let newProductList = [...productList];
+    newProductList.map((products) => {
+      products.quantity = 0;
+    });
+    setProductList(newProductList);
+    setTotalAmount(0);
+  };
+  const addItem = (name,price) => {
+    let newProductList = [...productList];
+    newProductList.push({
+      price: price,
+      name: name,
+      quantity: 0,
+    });
+  
+    setProductList(newProductList);
+    setTotalAmount(0);
+  };
+  const removeItem = (index) => {
+    let newProductList = [...productList];
+    let newTotalAmount = totalAmount;
+    newTotalAmount -=
+      newProductList[index].quantity * newProductList[index].price;
+    newProductList.splice(index, 1);
+    
+    setProductList(newProductList);
+    setTotalAmount(newTotalAmount);
+  };
+
+
+  return (
+    <>
+      <Navbar/> 
+      <main className='container' margin ='top'>
+        <AddItem addItem={addItem}/>
+      <ProductList productList={productList} incrementQuantity={incrementQuantity} decrementQuantity={decrementQuantity} removeItem={removeItem} />
+      </main>
+      <Footer totalAmount={totalAmount} resetQuantity={resetQuantity}/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+  
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+             Shopping_Cart
+            </a>
+            
+             </div>   
+              </nav>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
